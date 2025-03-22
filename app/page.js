544a605f3ed5325bce434daf41263868fa6170c2
memo-APis <adelvip222222@gmail.com>
2025-03-22 16:12:59 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>صباح الفليبو عاطف <h1>
+      <h1> spaho yabo atef  </h1>
+   
     </div>
   );
 }
